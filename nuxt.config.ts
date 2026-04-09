9d63bace9fe2d102af155b2e9703467144d0d5d0
FikFikk <fikri225456@gmail.com>
2026-04-09 10:03:50 +0700

    optimiez download instagram link

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,11 @@ export default defineNuxtConfig({
   },
 
   nitro: {
+    externals: {
+      inline: [
+        'instagram-url-direct'
+      ]
+    },
     rollupConfig: {
       output: {
         plugins: [
